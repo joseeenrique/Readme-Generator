e920de7200f63115b8fbf4faf63cc529619608f8
Jose <joseeenrique.jm@gmail.com>
2022-08-22 21:13:31 -0600

    started with index

---
+++ Develop/index.js
@@ -0,0 +1,17 @@
+// TODO: Include packages needed for this application
+const packages = require("packages");
+const fs = require("fs");
+const generateMarkdown = require ("./utils/generateMarkdown.js");
+console.log("Welcome")
+console.log("answer questions to continue")
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();

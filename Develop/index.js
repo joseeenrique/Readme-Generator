5960064d66a573fa583e57d3a5fb97749bb26272
Jose <joseeenrique.jm@gmail.com>
2022-08-22 21:25:53 -0600

    adding functions

--- Develop/index.js
+++ Develop/index.js
@@ -4,8 +4,51 @@ const fs = require("fs");
 const generateMarkdown = require ("./utils/generateMarkdown.js");
 console.log("Welcome")
 console.log("answer questions to continue")
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:"input",
+        name:"title",
+        message: "What is the title?",
+        validate: user_input => {
+            if (user_input){
+                return true;
+            } else{
+                console.log("enter title");
+                return false;
+
+            }
+        }
+    },
+
+{
+    type: "input",
+    name: "description",
+    message: "what is your page about?",
+    validate: user_description =>{
+        if (user_description){
+            return true;
+        } else{
+            console.log("enter description");
+            return false;
+        }
+    }
+},
+
+{
+    type:"input",
+    name:"title",
+    message: "What is the title?",
+    validate: user_input => {
+        if (user_input){
+            return true;
+        } else{
+            console.log("enter title");
+            return false;
+
+        }
+    }
+},
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}

418a000adbdd23f9421a6f22702b62b965fd0743
Jose <joseeenrique.jm@gmail.com>
2022-08-22 21:55:30 -0600

    started function to init app

--- Develop/index.js
+++ Develop/index.js
@@ -2,6 +2,7 @@
 const packages = require("packages");
 const fs = require("fs");
 const generateMarkdown = require ("./utils/generateMarkdown.js");
+const { default: inquirer } = require("inquirer");
 console.log("Welcome")
 console.log("answer questions to continue")
 const questions = [
@@ -9,8 +10,8 @@ const questions = [
         type:"input",
         name:"title",
         message: "What is the title?",
-        validate: user_input => {
-            if (user_input){
+        validate: user_license => {
+            if (user_license){
                 return true;
             } else{
                 console.log("enter title");
@@ -64,14 +65,99 @@ const questions = [
     }
 },
 
+{
+    type:"checkbox",
+    name:"license",
+    message: "choose license",
+    choices: ["MPL 2.0","APACHE","MIT","NONE"]
+    validate: user_license => {
+        if (user_license){
+            return true;
+        } else{
+            console.log("Select license");
+            return false;
 
+        }
+    }
+},
+{
+    type:"input",
+    name:"contribution",
+    message: "how can others contribute?",
+    validate: user_contribution => {
+        if (user_contribution){
+            return true;
+        } else{
+            console.log("provide others on how to contribute");
+            return false;
+
+        }
+    }
+},
+{
+    type:"input",
+    name:"test",
+    message: "how to test",
+    validate: user_test => {
+        if (user_test){
+            return true;
+        } else{
+            console.log("how to test");
+            return false;
+
+        }
+    }
+},
+
+{
+    type:"input",
+    name:"github",
+    message: "What is your github username?",
+    validate: user_github => {
+        if (user_github){
+            return true;
+        } else{
+            console.log("enter github username");
+            return false;
+
+        }
+    }
+},
+
+{
+    type:"input",
+    name:"email",
+    message: "What is your email?",
+    validate: user_email => {
+        if (user_email){
+            return true;
+        } else{
+            console.log("enter email");
+            return false;
+
+        }
+    }
+},
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err){
+            return console.log(err);
+        }
+        console.log("Congrats. here is a prew=view of your README")
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(userInput){
+        console.log(userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+    });
+};
 
 // Function call to initialize app
 init();

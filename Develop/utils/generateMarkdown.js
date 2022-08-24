1056c8d25f45f960658dd45702575ea202a382d0
Jose <joseeenrique.jm@gmail.com>
2022-08-23 21:53:25 -0600

    added functions for license

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,16 +1,55 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-function 
-
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licenseType = license.license; 
+  let yourLicense = ''
+  if(licenseType === 'MIT') {
+    yourLicense = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  } else if (licenseType === 'GPLv3') {
+    yourLicense = `![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+  } else if (licenseType === 'GPL') {
+    yourLicense = `![GPL license](https://img.shields.io/badge/License-GPL-blue.svg)`
+  } else {
+    license.license = "N/A"
+  }
+  return yourLicense;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseType = license.license; 
+  let yourLicense = ''
+  if(licenseType === 'MIT') {
+    // need to add license links inside parenthesis
+    yourLicense = `![License: MIT]()`
+  } else if (licenseType === 'GPLv3') {
+    yourLicense = `![GPLv3 license]()`
+  } else if (licenseType === 'GPL') {
+    yourLicense = `![GPL license]()`
+  } else {
+    license.license = "N/A"
+  }
+  return yourLicense;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseType = license.license; 
+  let yourLicense = ''
+  if(licenseType === 'MIT') {
+    // need to add license section inside parenthesis
+    yourLicense = `![License: MIT]()`
+  } else if (licenseType === 'GPLv3') {
+    yourLicense = `![GPLv3 license]()`
+  } else if (licenseType === 'GPL') {
+    yourLicense = `![GPL license]()`
+  } else {
+    license.license = "N/A"
+  }
+  return yourLicense;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
